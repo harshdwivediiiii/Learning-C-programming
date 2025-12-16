45f9ce691eac177109494dd8045c94b84f82e05d
harshdwivediiiii <harshvardhandwivedi18@gmail.com>
2025-12-16 14:07:21 +0530

    relational opreator

---
+++ chapter 2/relationalopreator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+printf("%d \n", 5==5);
+printf("%d \n", 7<3);
+printf("%d \n", 3<4);
+printf("%d \n", 4<8);
+printf("%d \n", 5<=5);
+printf("%d \n", 4>3);
+printf("%d \n", 3>4);
+printf("%d \n", 4>4);
+printf("%d \n", 4>=4);
+printf("%d \n", 4!=4);
+printf("%d \n", 3!=4);
+return 0;
+}
