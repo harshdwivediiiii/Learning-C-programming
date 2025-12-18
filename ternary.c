581df1f34718043cbd9dc5dd381ddcc3cc98022c
harshdwivediiiii <harshvardhandwivedi18@gmail.com>
2025-12-18 20:23:53 +0530

    fixed the conflicts

--- ternary.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h> //stdio
-int main() {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    age >= 18 ? printf("You are an adult\n") :
-    (age >= 13 && age < 18 ? printf("teenager\n") : printf("You are a child\n"));
-    return 0;
-}
