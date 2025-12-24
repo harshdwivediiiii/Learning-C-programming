5e9d49da3f422c5ef01332d7e1f2a7d28eee4214
harshdwivediiiii <harshvardhandwivedi18@gmail.com>
2025-12-24 10:14:18 +0530

    function printiing hello world

---
+++ chapter-5/functionhello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Function declaration/prototype
+void printHello() ;
+int main() {
+    printHello(); // Function call
+    return 0;
+}
+
+// Function definition
+
+void printHello() {
+    printf("Hello, World!\n");
+}
